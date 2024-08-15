5b4fe69381fbcd45f0d54dfc3dd95a0402bc5dd8
Godson okoye <161264678+Godson330@users.noreply.github.com>
2024-08-15 05:28:36 -0700

    Update newapptoken.test.js

--- newapptoken.test.js
+++ newapptoken.test.js
@@ -1,7 +1,7 @@
 it('should refresh an expired token', async function() {
   const res = await request(app)
-    .post('/refresh-token') // Replace with your actual token refresh endpoint
-    .set('Authorization', `Bearer ${refreshToken}`) // Include the expired token 
+    .post('/refresh-token')
+    .set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUyOWRkOTA3LWNmMTUtNGI4Yy04MzU4LTIwM2ZkODgxZjFjZCIsInVzZXJuYW1lIjoiam9lIiwiaWF0IjoxNzIzMTMwNDg4fQ.L0veYqRdR6-Lh0L8UnSxNY4OWH8jES1SvGdxDKXI-Bg`) // Include the expired token
     .send(); 
 
   expect(res.statusCode).toEqual(200);
